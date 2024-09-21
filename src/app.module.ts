b7e9fd492e8f1ddb6b8eb9d92f2390302b8f3a1e
DanielEvaristo <evaristo.camargo.daniel@gmail.com>
2024-09-21 16:55:31 -0600

    BD y ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [EmployeesModule, ProductsModule],
-  
+  imports: [TypeOrmModule.forRoot({
+    type: "postgres",
+    host: process.env.host,
+    port: +process.env.port,
+    username: "postgres",
+    password: "TheBestPassword",
+    database: process.env.name,
+    entities: [],
+    synchronize: true,
+  }), EmployeesModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
