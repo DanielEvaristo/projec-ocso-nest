f5723b36f6ffabab7b95d977ce26dd5883ec2165
DanielEvaristo <evaristo.camargo.daniel@gmail.com>
2024-09-28 22:41:01 -0600

    Hasheo

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,18 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
+  @Post("signup")
   singup(@Body() createUserhDto: CreateUserDto){
-    this.authService.registerUser(createUserhDto)
+    return this.authService.registerUser(createUserhDto)
+  }
+
+  @Post("login")
+  login(@Body() createUserDto: CreateUserDto){
+    return this.authService.loginUser(createUserDto)
   }
 }
