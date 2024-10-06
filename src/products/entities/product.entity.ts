85cdf1156591079e336033b770ab86dcf018bf96
DanielEvaristo <evaristo.camargo.daniel@gmail.com>
2024-10-05 23:21:30 -0600

    More swagger implementacion

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,15 +1,29 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Provider } from "src/providers/entities/provider.entity";
 import { Entity,Column, ManyToOne, PrimaryGeneratedColumn, IsNull, JoinColumn } from "typeorm";
 @Entity()
 export class Product {
     @PrimaryGeneratedColumn("uuid")
     productId: string;
+
+    @ApiProperty({
+        default: "Barcel Rufles"
+    })
     @Column({type: "text"})
     productName: string;
+
+    @ApiProperty({
+        default: 14.50
+    })
     @Column({type: "float"})
     price: number;
+
+    @ApiProperty({
+        default: 13
+    })
     @Column({type: "int"})
     countSeal: number;
+
     @ManyToOne(() => Provider, (provider) => provider.products,{
         //eager: true,
     })
